39ef1a72f491737f19294620ad18fbc14450f5f5
Gabriel Gomez <gomez.gabriel26293@gmail.com>
2026-06-02 23:27:42 -0300

    Mude el codigo de app.ts a ticket.service.ts para delegar logica, y agregue Fix: Corrección del autoincremental de IDs en el TicketService. Por el momento no borrare los comentarios de app.ts #mudado

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TicketService, Ticket } from './ticket.service';
 
+//mudado#1 a service
+/*
 interface Ticket{
   id_ticket: number;
   solicitante: string;
   descripcion: string;
   equipoAsignado: string;
   abierto: boolean;
-}
+}*/
 
 @Component({
   selector: 'app-root',
@@ -19,16 +22,24 @@ export class App { //clase principal
   
   tituloSistema: string = 'Gigo - Gestion de Incidente';
 
-  //lista por ahora vacia
-  listaTickets: Ticket[] = [];
+  /*#mudado2 a service lista por ahora vacia
+  listaTickets: Ticket[] = []; #mudado2 */
 
   //auxiliares
   nuevoSolicitante: string = '';
   nuevoEquipo: string = '';
   nuevaDescripcion: string = '';
 
+  //uso el constructor de mi clase para aplicar la inyeccion de dependencia > Parte de la mudanza a service
+  constructor(private miServicio: TicketService) {}
+
+  //otra parte de la mudanza > uso el getter para que el html pueda leer mi lista
+  get listaTickets(): Ticket[] {
+    return this.miServicio.listaTickets;
+  }
+
   //objeto crear > valido datos vacios y los guardo 
-  crearTicket (){
+  /*crearTicket (){
     //valido datos vacios
     if (this.nuevoSolicitante == '' || this.nuevaDescripcion == ''){
       alert ("Por favor, complete al menos el solicitante y la descripcion.");
@@ -49,7 +60,7 @@ export class App { //clase principal
 
 
 
-    //pusheo el ticket en la lista o sea en nuevo incidente
+    //pusheo el ticket en la lista (creo incidente y lo mando)
     this.listaTickets.push(nuevoIncidente);
 
     //limpio
@@ -72,6 +83,36 @@ export class App { //clase principal
   //metodo para "eliminar" con filter, ver si lo dejamos
   eliminarTicket(id_ticket_buscado: number){
     this.listaTickets = this.listaTickets.filter(ticket => ticket.id_ticket !== id_ticket_buscado);
+  }*/
+
+  //parte de la mudanza hace esto: que la pantalla solo avisa y el servicio trabaja
+  crearTicket() {
+    if (this.nuevoSolicitante == '' || this.nuevaDescripcion == '') {
+      alert ("Por favor, complete al menos el solicitante y la descripcion.");
+      return;  
+    }
+
+  
+    this.miServicio.agregarTicket(
+      this.nuevoSolicitante, 
+      this.nuevoEquipo, 
+      this.nuevaDescripcion
+    );
+
+   
+    this.nuevoSolicitante = '';
+    this.nuevoEquipo = '';
+    this.nuevaDescripcion = '';
+  }
+
+
+  //resolverTicket y eiliminar Ticket lo mude a service.
+  resolverTicket(idBuscado: number) {
+    this.miServicio.resolverTicket(idBuscado);
+  }
+
+  eliminarTicket(id_ticket_buscado: number) {
+    this.miServicio.eliminarTicket(id_ticket_buscado);
   }
 
 
---
+++ src/app/ticket.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+export interface Ticket{
+  id_ticket: number;
+  solicitante: string;
+  descripcion: string;
+  equipoAsignado: string;
+  abierto: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TicketService {
+
+  listaTickets: Ticket[] = [];
+
+  //al mudar la creacion a service no puedo leer pantalla entonces paso datos por parametros
+
+  contadorId: number = 1;
+  agregarTicket(solicitanteIngresado: string, equipoIngresado: string, descripcionIngresada: string) {
+    const nuevoIncidente: Ticket = {
+      id_ticket: this.contadorId,
+      solicitante: solicitanteIngresado,
+      equipoAsignado: equipoIngresado,
+      descripcion: descripcionIngresada,
+      abierto: true 
+    };
+    
+    this.listaTickets.push(nuevoIncidente);
+    this.contadorId++;
+  }
+
+  resolverTicket(idBuscado: number) {
+    const ticketEncontrado = this.listaTickets.find(ticket => ticket.id_ticket === idBuscado);
+    if (ticketEncontrado) {
+      ticketEncontrado.abierto = false;
+    }
+  }
+
+  eliminarTicket(id_ticket_buscado: number) {
+    this.listaTickets = this.listaTickets.filter(ticket => ticket.id_ticket !== id_ticket_buscado);
+  }
+  
+}
