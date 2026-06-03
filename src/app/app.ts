0d4803aee5efddc6031768513813d640d8e9190a
Gabriel Gomez <gomez.gabriel26293@gmail.com>
2026-06-02 21:14:05 -0300

    Agrego la funcion de eliminar incidentes (CRUD completado)

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 interface Ticket{
-  id: number;
+  id_ticket: number;
   solicitante: string;
   descripcion: string;
   equipoAsignado: string;
@@ -15,7 +15,7 @@ interface Ticket{
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App { //clase principal
   
   tituloSistema: string = 'Gigo - Gestion de Incidente';
 
@@ -27,23 +27,27 @@ export class App {
   nuevoEquipo: string = '';
   nuevaDescripcion: string = '';
 
-  //valido datos vacios y los guardo
+  //objeto crear > valido datos vacios y los guardo 
   crearTicket (){
     //valido datos vacios
     if (this.nuevoSolicitante == '' || this.nuevaDescripcion == ''){
       alert ("Por favor, complete al menos el solicitante y la descripcion.");
       return;  
     }
+    //Tengo ver si mas adelante le meto otra valadacion
+
 
     //mi objeto para crear
     const nuevoIncidente: Ticket = {
 
-      id: this.listaTickets.length + 1,
+      id_ticket: this.listaTickets.length + 1,
       solicitante: this.nuevoSolicitante,
       equipoAsignado: this.nuevoEquipo,
       descripcion: this.nuevaDescripcion,
       abierto: true
     }
+
+
 
     //pusheo el ticket en la lista o sea en nuevo incidente
     this.listaTickets.push(nuevoIncidente);
@@ -53,6 +57,21 @@ export class App {
     this.nuevoEquipo='';
     this.nuevaDescripcion='';
 
+  }
+
+  //mi otro objeto resolver
+  resolverTicket (idBuscado: number) {
+    //uso find pero equivale a select *from where id_ticket = x si uso bd
+    const ticketEncontrado = this.listaTickets.find (ticket => ticket.id_ticket === idBuscado); //SELECT * FROM listaTickets WHERE id_ticket = @idBuscado
+
+      if(ticketEncontrado){
+        ticketEncontrado.abierto =false;
+      }
+  }
+
+  //metodo para "eliminar" con filter, ver si lo dejamos
+  eliminarTicket(id_ticket_buscado: number){
+    this.listaTickets = this.listaTickets.filter(ticket => ticket.id_ticket !== id_ticket_buscado);
   }
 
 
